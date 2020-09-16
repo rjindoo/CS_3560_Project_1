c9218cd4c8016180084b82444a91dd1c2a1265fe
rjindoo <platypus2k1@gmail.com>
2020-09-16 15:26:16 -0700

    Created basic files and .gitignore for Project

--- src/DriverClass.java
+++ src/DriverClass.java
@@ -1,8 +1,10 @@
 public class DriverClass {
     public static void main(String[] args){
-        // Create student and question objects to simulate voting simulator
+        // Create student and question objects voting simulator
         Student a = new Student();
+        Question q = new MultipleChoiceQuestion();
 
+        System.out.println(q.getQuestionText());
         System.out.println(a.getStudentID());
         System.out.println(a.getStudentAnswer());
 
--- src/MultipleChoiceQuestion.java
+++ src/MultipleChoiceQuestion.java
@@ -3,13 +3,17 @@ import java.util.Map;
 
 public class MultipleChoiceQuestion implements Question {
 
+    private int counter = 0;
+
     private String[] questionBank = {
             "Multiple Choice"
     };
 
     @Override
     public String getQuestionText() {
-        return null;
+        String question = questionBank[counter];
+        ++counter;
+        return question;
     }
 
 
